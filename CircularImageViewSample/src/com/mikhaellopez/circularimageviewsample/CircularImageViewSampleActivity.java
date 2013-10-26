6d83b9b3819760670eb4fb14c5416d9e26c0980d
Lopez Mikhael <lopez.mikhael@gmail.com>
2013-10-26 18:04:37 +0200

    Add Sample

---
+++ CircularImageViewSample/src/com/mikhaellopez/circularimageviewsample/CircularImageViewSampleActivity.java
@@ -0,0 +1,14 @@
+package com.mikhaellopez.circularimageviewsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CircularImageViewSampleActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sample);
+	}
+
+}
