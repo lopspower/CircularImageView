2a639f1b9226b01b94772b5b39ef2f237e8d4852
Lopez Mikhael <lopez.mikhael@gmail.com>
2014-05-12 11:54:02 +0200

    Allow dimension of border width

--- CircularImageView/src/com/mikhaellopez/circularimageview/CircularImageView.java
+++ CircularImageView/src/com/mikhaellopez/circularimageview/CircularImageView.java
@@ -41,8 +41,9 @@ public class CircularImageView extends ImageView {
 		TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.CircularImageView, defStyle, 0);
 
 		if(attributes.getBoolean(R.styleable.CircularImageView_border, true)) {
-			setBorderWidth(attributes.getColor(R.styleable.CircularImageView_border_width, 4));
-			setBorderColor(attributes.getInt(R.styleable.CircularImageView_border_color, Color.WHITE));
+			int defaultBorderSize = (int) (4 * getContext().getResources().getDisplayMetrics().density + 0.5f);
+			setBorderWidth(attributes.getDimensionPixelOffset(R.styleable.CircularImageView_border_width, defaultBorderSize));
+			setBorderColor(attributes.getColor(R.styleable.CircularImageView_border_color, Color.WHITE));
 		}
 
 		if(attributes.getBoolean(R.styleable.CircularImageView_shadow, false))
