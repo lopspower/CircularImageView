ffabe87aad15a7877a8da83ba7e22ffda4c11234
Mikhael Lopez <lopez.mikhael@gmail.com>
2015-12-07 22:36:39 +0100

    New Sample for release 2.1.0

--- circularimageview-example/src/main/java/com/mikhaellopez/circularimageviewsample/MainActivity.java
+++ circularimageview-example/src/main/java/com/mikhaellopez/circularimageviewsample/MainActivity.java
@@ -1,19 +1,73 @@
 package com.mikhaellopez.circularimageviewsample;
 
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.widget.SeekBar;
+
+import com.larswerkman.lobsterpicker.OnColorListener;
+import com.larswerkman.lobsterpicker.sliders.LobsterShadeSlider;
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 /**
  * Created by Mikhael LOPEZ on 09/10/15.
  */
 public class MainActivity extends AppCompatActivity {
 
+    private CircularImageView circularImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        circularImageView = (CircularImageView) findViewById(R.id.circularImageView);
+
+        // BORDER
+        ((SeekBar) findViewById(R.id.seekBarBorderWidth)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                circularImageView.setBorderWidth(progress * (int) getResources().getDisplayMetrics().density);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
+        // AMPLITUDE
+        ((SeekBar) findViewById(R.id.seekBarShadowRadius)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                circularImageView.setShadowRadius(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        // COLOR
+        ((LobsterShadeSlider) findViewById(R.id.shadeslider)).addOnColorListener(new OnColorListener() {
+            @Override
+            public void onColorChanged(@ColorInt int color) {
+                circularImageView.setBorderColor(color);
+                circularImageView.setShadowColor(color);
+            }
+
+            @Override
+            public void onColorSelected(@ColorInt int color) {
+            }
+        });
     }
 
 }
